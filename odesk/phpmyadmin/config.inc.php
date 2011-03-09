263dade5ea00784248d5099fb254457f6a42effc
Andrew Miller <socrates1024@gmail.com>
2011-03-08 18:58:18 -0600

    committing the phpmyadmin template
    Signed-off-by: Andrew Miller <socrates1024@gmail.com>

---
+++ odesk/phpmyadmin/config.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Servers configuration */
+$i = 0;
+
+$i++;
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = '%{DBUSER}';
+$cfg['Servers'][$i]['password'] = '%{PASSWORD}';
+$cfg['Servers'][$i]['only_db'] = '%{DATABASE}';
+
+$cfg['Servers'][$i]['AllowDeny']['order'] = '';
+
+$cfg['Servers'][$i]['AllowDeny']['rules'] = array();
+
+
+/* End of servers configuration */
+
+?>

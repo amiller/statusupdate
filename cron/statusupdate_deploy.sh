953c2f228ad9c429018c51bc5946586fd418f6fa
Andrew Miller <socrates1024@gmail.com>
2011-07-06 12:45:17 -0500

    Fixed cron job deployer
    Signed-off-by: Andrew Miller <socrates1024@gmail.com>

--- cron/statusupdate_deploy.sh
+++ cron/statusupdate_deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export HOME=/home/soc1024c/
 source ~/.bash_profile
-pushd /home/soc1024c/statusupdate/statusupdate
-./jetz.py 8180 &
+pushd /home/soc1024c/statusupdate/server
+./server_statusupdate.py 8180 &
 popd
